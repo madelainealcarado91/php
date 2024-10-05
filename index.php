e27d548dfbd722fd2d46f12c1c0415bed1166037
madelainealcarado <madelainealcarado91@gmail.com>
2024-10-05 01:47:54 -0700

    second commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<?php 
+$firstName1 = "Madelaine";
+$lastName1 = "Alcarado";
+$Age1 = 16;
+$Address1 = "Antipolo";
+$Contact1 = "08721682687";
+
+$firstName2 = "Michaela";
+$lastName2 = "Moldon";
+$Age2 = 15;
+$Address2 = "Antipolo";
+$Contact2 = "08747382687";
+
+$firstName3 = "Chelse0a";
+$lastName3 = "Modesto";
+$Age3 = 14;
+$Address3 = "Antipolo";
+$Contact3 = "087216687";
+
+$firstName4 = "Kathleen";
+$lastName4 = "Acopiado";
+$Age4 = 16;
+$Address4 = "Antipolo";
+$Contact4 = "08721647687";
+?> 
+<ul>
+    <li>First Name: <?= $firstName1 ?></li>
+    <li>Last Name: <?= $lastName1 ?></li>
+    <li>Age: <?= $Age1 ?></li>
+    <li>Address: <?= $Address1 ?></li>
+    <li>Contact: <?= $Contact1 ?></li>
+<br>
+    <li>First Name: <?= $firstName2 ?></li>
+    <li>Last Name: <?= $lastName2 ?></li>
+    <li>Age: <?= $Age2 ?></li>
+    <li>Address: <?= $Address2 ?></li>
+    <li>Contact: <?= $Contact2 ?></li>
+    <br>
+    <li>First Name: <?= $firstName3 ?></li>
+    <li>Last Name: <?= $lastName3 ?></li>
+    <li>Age: <?= $Age3 ?></li>
+    <li>Address: <?= $Address3 ?></li>
+    <li>Contact: <?= $Contact3 ?></li>
+    <br>
+    <li>First Name: <?= $firstName4 ?></li>
+    <li>Last Name: <?= $lastName4 ?></li>
+    <li>Age: <?= $Age4 ?></li>
+    <li>Address: <?= $Address4 ?></li>
+    <li>Contact: <?= $Contact4 ?></li>
+
+
+
+
+</body>
+</html>
